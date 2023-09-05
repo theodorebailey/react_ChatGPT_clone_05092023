fd3a086c3750508ce294df1009d4dea4fcef2e87
Theodore Bailey <tawbailey@hotmail.com>
2023-09-05 09:41:01 +0100

    project commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <div className="upperside">
+          <div className="upperside-top">
+            <img src={gptLogo} alt="logo" className='logo' /><span className='brand'>ChatGPT</span>
+            <button className="mid-btn"><img src={addBtn} alt="" className='add-btn' />New Chat</button>
+          </div>
+          <div className="upperside-bottom">
+              <button className="query"><img src={msgIcon} alt="query" className='' />What is programming ?</button>
+              <button className="query"><img src={msgIcon} alt="" className='' />How to use an API</button>
+            </div>
+        </div>
+        <div className="lowerside">
+          <div className="list-items"><img src={home} alt="" className="list-items-img" />Home</div>
+          <div className="list-items"><img src={saved} alt="" className="list-items-img" />Saved</div>
+          <div className="list-items"><img src={rocket} alt="" className="list-items-img" />Upgrade to Pro</div>
+        </div>
+      </div>
+      <div className="main"></div>
     </div>
   );
 }
