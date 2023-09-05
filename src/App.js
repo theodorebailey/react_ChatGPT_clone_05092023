e0c6b7ce45bcc7103feba194f45b4a148c7aa6a1
Theodore Bailey <tawbailey@hotmail.com>
2023-09-05 15:51:44 +0100

    lost interest in style of teaching at 50mins

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import msgIcon from './assets/message.svg';
 import home from './assets/home.svg';
 import saved from './assets/bookmark.svg';
 import rocket from './assets/rocket.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
+
 
 function App() {
   return (
@@ -13,12 +17,12 @@ function App() {
         <div className="upperside">
           <div className="upperside-top">
             <img src={gptLogo} alt="logo" className='logo' /><span className='brand'>ChatGPT</span>
-            <button className="mid-btn"><img src={addBtn} alt="" className='add-btn' />New Chat</button>
           </div>
+          <button className="mid-btn"><img src={addBtn} alt="" className='add-btn' />New Chat</button>
           <div className="upperside-bottom">
-              <button className="query"><img src={msgIcon} alt="query" className='' />What is programming ?</button>
-              <button className="query"><img src={msgIcon} alt="" className='' />How to use an API</button>
-            </div>
+            <button className="query"><img src={msgIcon} alt="query" className='' />What is programming ?</button>
+            <button className="query"><img src={msgIcon} alt="" className='' />How to use an API</button>
+          </div>
         </div>
         <div className="lowerside">
           <div className="list-items"><img src={home} alt="" className="list-items-img" />Home</div>
@@ -26,7 +30,28 @@ function App() {
           <div className="list-items"><img src={rocket} alt="" className="list-items-img" />Upgrade to Pro</div>
         </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img src={userIcon} alt="" />
+            <p className="txt">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+          <div className="chat">
+            <img src={gptImgLogo} alt="" />
+            <p className="txt">
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
+        <div className="chat-footer">
+          <div className="inp">
+            <input type="text" placeholder="Send message." /><button className="send"><img src={sendBtn} alt="send" /></button>
+            <p>ChatGPT may produce incorrect results..</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
